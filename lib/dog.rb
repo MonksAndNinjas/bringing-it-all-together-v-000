1268f679a5603c53a8d8590f4ff698538689f39e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 23:58:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,13 @@ class Dog
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
   end
 
 end
