a3758fd99f558d66bb42148f28a8e090fd72c3f8
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 23:23:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,4 +28,12 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
+
 end
